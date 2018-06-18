6fa03ebf4b993c52bb89f465c32eaae9dc0e4bfa
Andre Youn <andre.youn@usp.br>
2018-06-18 10:34:02 -0300

    Aula5 - funções for

---
+++ Aula5 - Funções/exercicio-for.js
@@ -0,0 +1,16 @@
+let i=1;
+
+while (i <= 10){
+    console.log(i);
+    i++;
+}
+
+console.log("------------");
+// como fazer um for exatamente equivalente a esse while. 
+// a estrutura do for é semelhante ao while. 
+
+// inicialização condição e atualização para for e while
+// esse let i é exclusivo do for
+for (let i = 1; i <= 10; i++){
+    console.log(i);    
+}
